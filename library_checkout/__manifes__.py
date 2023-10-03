cdec4bcfac77d518cf0fd83f8158602e48494b3a
Eduardo Vieira <eduardo.susan@gmail.com>
2023-10-03 17:47:26 -0600

    adding checkout module

---
+++ library_checkout/__manifes__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Library Book Checkout",
+    "description": "Members can borrow books from the library.",
+    "author": "Daniel Reis",
+    "license": "AGPL-3",
+    "depends": ["library_member"],
+    "data": [
+        "security/ir.model.access.csv",
+        
+        "views/library_menu.xml",
+        "views/checkout_view.xml",
+    
+    ],
+}
